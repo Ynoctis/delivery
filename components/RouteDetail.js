83eb249e90a52a27157998ed124710f7862e144f
Nigel Yang <nigelyang611@hotmail.com>
2024-01-30 17:00:54 -0500

    Finished Route Detail

--- components/RouteDetail.js
+++ components/RouteDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Pressable, StyleSheet, Dimensions, Linking } from 'react-native';
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome6, MaterialCommunityIcons, Feather, MaterialIcons } from '@expo/vector-icons';
 import MapView, { Marker } from 'react-native-maps';
 
 
@@ -66,23 +66,57 @@ const RouteDetail = ({ route, navigation }) => {
             <View style={styles.footer}>
                 <View>
                     <View style={{ marginBottom: 15, flexDirection: 'row', }}>
-                        <View style={{ flex: 4, alignItems: 'center',}}>
+                        <View style={{ flex: 4, alignItems: 'center', }}>
                             <Text style={{ fontWeight: 'bold', fontSize: 40, marginTop: 15, }}>{distance}</Text>
-                            <Text style={{ }}>Route</Text>
-                            <Text style={{borderColor: 'black', borderWidth: 2, borderRadius: 15, paddingLeft: 13, paddingRight: 10}}>House</Text>
+                            <Text style={{}}>Route</Text>
+                            <Text style={{ borderColor: 'black', borderWidth: 2, borderRadius: 15, paddingLeft: 13, paddingRight: 10 }}>House</Text>
                         </View>
-                        <View style={{ flex: 6, marginTop: 15, marginRight: 5}}>
-                            <Text style={{fontWeight: '500', fontSize: 20}}>{name}</Text>
+                        <View style={{ flex: 6, marginTop: 15, marginRight: 5 }}>
+                            <Text style={{ fontWeight: '500', fontSize: 20 }}>{name}</Text>
                             <Text style={{ marginBottom: 5, fontSize: 12 }} >+14164164166</Text>
                             <Text style={{ fontWeight: '500', fontSize: 17, marginBottom: 5 }}>Unit 123</Text>
                             <Text numberOfLines={2} style={{ fontWeight: '500', fontSize: 16, }}>{address}</Text>
                         </View>
                     </View>
-                    <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-                        <Pressable onPress={handleCallPress} style={{backgroundColor: 'lightgray', paddingHorizontal: 40, borderRadius: 15, paddingVertical: 10}}><Text>Call</Text></Pressable>
-                        <Pressable onPress={handleTextPress} style={{backgroundColor: 'lightgray', paddingHorizontal: 40, borderRadius: 15, paddingVertical: 10}}><Text>Text</Text></Pressable>
-                        <Pressable onPress={handleNavPress} style={{backgroundColor: 'lightgray', paddingHorizontal: 40, borderRadius: 15, paddingVertical: 10}}><Text>Nav</Text></Pressable>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                        <Pressable onPress={handleCallPress} style={{ backgroundColor: 'lightgray', paddingHorizontal: 30, borderRadius: 15, paddingVertical: 10, flexDirection: 'row' }}>
+                            <FontAwesome6 name="phone" size={20} color="black" style={{ marginRight: 10 }} />
+                            <Text>Call</Text>
+                        </Pressable>
+                        <Pressable onPress={handleTextPress} style={{ backgroundColor: 'lightgray', paddingHorizontal: 30, borderRadius: 15, paddingVertical: 10, flexDirection: 'row' }}>
+                            <MaterialIcons name="message" size={20} color="black" style={{ marginRight: 10 }} />
+                            <Text>Text</Text>
+                        </Pressable>
+                        <Pressable onPress={handleNavPress} style={{ backgroundColor: 'lightgray', paddingHorizontal: 30, borderRadius: 15, paddingVertical: 10, flexDirection: 'row' }}>
+                            <MaterialIcons name="navigation" size={20} color="black" style={{ marginRight: 10 }} />
+                            <Text>Nav</Text>
+                        </Pressable>
                     </View>
+                    <View style={{marginVertical: 10, marginHorizontal: 20}}>
+                        <Text>Assigned Time: 2023-12-17 21:47:28</Text>
+                        <Text>Delivery Type: Regular</Text>
+                        <Text>Delivery Attempt: 1 out of 3</Text>
+                        <Text>Delivery by: 2023-12-18 21:47:28</Text>
+                        <Text>Package on hold over 1000 hours</Text>
+                    </View>
+                    <View style={{marginVertical: 10, marginHorizontal: 20}}>
+                        <Text>Buzz: N/A</Text>
+                    </View>
+                    <View style={{marginVertical: 10, marginHorizontal: 20}}>
+                        <Text>Gated Community Code: 001</Text>
+                    </View>
+                    <View style={{marginVertical: 10, marginHorizontal: 20}}>
+                        <Text>{note}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', marginTop: 30}}>
+                        <Pressable style={{ flex: 3, backgroundColor: 'lightgray', paddingHorizontal: 30, borderRadius: 15, paddingVertical: 10, justifyContent: 'center', alignItems: 'center', marginHorizontal: 20 }}>
+                            <Text>Failed</Text>
+                        </Pressable>
+                        <Pressable style={{ flex: 7, backgroundColor: 'lightgray', paddingHorizontal: 30, borderRadius: 15, paddingVertical: 10, justifyContent: 'center', alignItems: 'center', marginRight: 20 }}>
+                            <Text>Delivered</Text>
+                        </Pressable>
+                    </View>
+
                 </View>
             </View>
 
