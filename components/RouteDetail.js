1cbfa80f18a4d4bf4fc362a9b1c37f1ae5eadec4
Nigel Yang <nigelyang611@hotmail.com>
2024-01-30 15:35:03 -0500

    Added info and call, text, nav buttons, 50% complete

--- components/RouteDetail.js
+++ components/RouteDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Pressable, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, Pressable, StyleSheet, Dimensions, Linking } from 'react-native';
 import { FontAwesome6 } from '@expo/vector-icons';
 import MapView, { Marker } from 'react-native-maps';
 
@@ -15,7 +15,26 @@ const RouteDetail = ({ route, navigation }) => {
         // fetch geocoding api to translate address to latitude and longitude
     }
 
-    const staticCoordinates = { latitude: 37.7749, longitude: -122.4194 }
+    const staticCoordinates = { latitude: 37.7749, longitude: -122.4194 };
+
+    // static for now, can be imported via route.params when API is given
+    const phoneNumber = '+14164164166'
+    const destination = '115 Ravel Road'
+
+    const handleCallPress = () => {
+        const phoneUrl = `tel:${phoneNumber}`;
+        Linking.openURL(phoneUrl)
+    }
+
+    const handleTextPress = () => {
+        const textUrl = `sms:${phoneNumber}`
+        Linking.openURL(textUrl)
+    }
+
+    const handleNavPress = () => {
+        const navUrl = `https://www.google.com/maps/dir/?api=1&destination=${destination}`;
+        Linking.openURL(navUrl)
+    }
 
     return (
         <View style={styles.container}>
@@ -33,8 +52,8 @@ const RouteDetail = ({ route, navigation }) => {
                 initialRegion={{
                     latitude: staticCoordinates.latitude,
                     longitude: staticCoordinates.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
+                    latitudeDelta: 0.0225,
+                    longitudeDelta: 0.0121,
                 }}
             >
                 <Marker
@@ -43,6 +62,30 @@ const RouteDetail = ({ route, navigation }) => {
                     description={address}
                 />
             </MapView>
+
+            <View style={styles.footer}>
+                <View>
+                    <View style={{ marginBottom: 15, flexDirection: 'row', }}>
+                        <View style={{ flex: 4, alignItems: 'center',}}>
+                            <Text style={{ fontWeight: 'bold', fontSize: 40, marginTop: 15, }}>{distance}</Text>
+                            <Text style={{ }}>Route</Text>
+                            <Text style={{borderColor: 'black', borderWidth: 2, borderRadius: 15, paddingLeft: 13, paddingRight: 10}}>House</Text>
+                        </View>
+                        <View style={{ flex: 6, marginTop: 15, marginRight: 5}}>
+                            <Text style={{fontWeight: '500', fontSize: 20}}>{name}</Text>
+                            <Text style={{ marginBottom: 5, fontSize: 12 }} >+14164164166</Text>
+                            <Text style={{ fontWeight: '500', fontSize: 17, marginBottom: 5 }}>Unit 123</Text>
+                            <Text numberOfLines={2} style={{ fontWeight: '500', fontSize: 16, }}>{address}</Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                        <Pressable onPress={handleCallPress} style={{backgroundColor: 'lightgray', paddingHorizontal: 40, borderRadius: 15, paddingVertical: 10}}><Text>Call</Text></Pressable>
+                        <Pressable onPress={handleTextPress} style={{backgroundColor: 'lightgray', paddingHorizontal: 40, borderRadius: 15, paddingVertical: 10}}><Text>Text</Text></Pressable>
+                        <Pressable onPress={handleNavPress} style={{backgroundColor: 'lightgray', paddingHorizontal: 40, borderRadius: 15, paddingVertical: 10}}><Text>Nav</Text></Pressable>
+                    </View>
+                </View>
+            </View>
+
             {/* <Text>{deliveryNumber}</Text>
             <Text>{name}</Text>
             <Text>{address}</Text>
@@ -57,24 +100,35 @@ const RouteDetail = ({ route, navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // alignItems: "center",
-        // justifyContent: "center",
         backgroundColor: 'white'
     },
     map: {
         width: "100%",
-        height: "100%",
+        height: Dimensions.get('window').height / 2.5
     },
     header: {
         position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
-        backgroundColor: 'rgba(255, 255, 255, 0.8)', // Use a semi-transparent background
-        // padding: 10,
-        zIndex: 2, // Make sure the header is above the map
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        zIndex: 2,
         paddingTop: Dimensions.get('window').height / 13.5,
     },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'white',
+        height: Dimensions.get('window').height / 1.5,
+        flex: 1,
+        borderRadius: 20,
+    },
+    deliveryItem: {
+        borderRadius: 10,
+        marginBottom: 20,
+    }
 });
 
 export default RouteDetail;
